e63060214b35c21cd6fd6608773fd20d8f8d0e65
Adriano Torres <adrianortorres@gmail.com>
2026-03-09 18:33:48 +1030

    disable local test runner

--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -1,10 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 class DriverTest {
 
-  @Test
+  @Disabled
   void shouldRunDriverWithValidJar() {
     assertDoesNotThrow(() -> Driver.main(new String[] {"witup-test-jar-1.0-SNAPSHOT.jar"}));
   }
