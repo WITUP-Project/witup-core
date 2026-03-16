4b24aa8391394709369958f799e43767e634eb05
Adriano Torres <adrianortorres@gmail.com>
2026-03-16 23:46:42 +1030

    fix virtual invoke not substituting properly, pass common-langs toBoolean(int,int,int)

---
+++ src/test/java/br/unb/cic/witup/samples/Bool.java
@@ -0,0 +1,21 @@
+package br.unb.cic.witup.samples;
+
+public class Bool {
+  // From apache commons used exclusively for testing. Hopefully we don't need
+  // to delete for licensing reasons?
+  public static boolean toBoolean(final Integer value, final Integer trueValue, final Integer falseValue) {
+    if (value == null) {
+      if (trueValue == null) {
+        return true;
+      }
+      if (falseValue == null) {
+        return false;
+      }
+    } else if (value.equals(trueValue)) {
+      return true;
+    } else if (value.equals(falseValue)) {
+      return false;
+    }
+    throw new IllegalArgumentException("The Integer did not match either specified value");
+  }
+}
