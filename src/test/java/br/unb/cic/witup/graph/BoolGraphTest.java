9586015103edf861d9a4af61c77914e141b69cc9
Adriano Torres <adrianortorres@gmail.com>
2026-03-21 11:22:25 +1030

    split toBoolean

---
+++ src/test/java/br/unb/cic/witup/graph/BoolGraphTest.java
@@ -0,0 +1,29 @@
+package br.unb.cic.witup.graph;
+
+import br.unb.cic.witup.analysis.graph.WITUpGraph;
+import br.unb.cic.witup.analysis.graph.node.ThrowStatementNode;
+import br.unb.cic.witup.analysis.graph.node.WITUpNode;
+import br.unb.cic.witup.testinfra.TestAnalysisContext;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BoolGraphTest {
+  @Test
+  public void toBooleanGraph() {
+    String methodSignature = "<br.unb.cic.witup.samples.Bool: boolean toBoolean(java.lang.Integer,java.lang.Integer,java.lang.Integer)>";
+
+    WITUpGraph cpg = TestAnalysisContext.getGraphs().get(methodSignature);
+    assertNotNull(cpg);
+
+    List<WITUpNode> throwNodes = cpg.getThrowNodes();
+    assertEquals(1, throwNodes.size());
+
+    List<WITUpNode> conditionNodes =
+            cpg.getThrowConditionNodes((ThrowStatementNode) throwNodes.get(0));
+    assertEquals(5, conditionNodes.size());
+  }
+}
---
+++ src/test/java/br/unb/cic/witup/solver/BoolSolverTest.java
@@ -0,0 +1,30 @@
+package br.unb.cic.witup.solver;
+
+import br.unb.cic.witup.testinfra.TestAnalysisContext;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BoolSolverTest {
+  @Test
+  public void toBooleanGraphSolution() {
+    String methodSignature = "<br.unb.cic.witup.samples.Bool: boolean toBoolean(java.lang.Integer,java.lang.Integer,java.lang.Integer)>";
+
+    SolverResult sol0 = TestAnalysisContext.getSolutions()
+            .get(methodSignature).getFirst();
+
+    assertTrue(sol0.isSat());
+    assertFalse(sol0.getBool("value_is_null"));
+    assertFalse(sol0.getBool("value.equals(trueValue)"));
+    assertFalse(sol0.getBool("value.equals(falseValue)"));
+
+    SolverResult sol1 = TestAnalysisContext.getSolutions()
+            .get(methodSignature).get(1);
+
+    assertTrue(sol1.isSat());
+    assertTrue(sol1.getBool("value_is_null"));
+    assertFalse(sol1.getBool("trueValue_is_null"));
+    assertFalse(sol1.getBool("falseValue_is_null"));
+  }
+}
