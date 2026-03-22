1953bd486fb0b532b5565c72921152587048287c
Adriano Torres <adrianortorres@gmail.com>
2026-03-23 00:42:22 +1030

    use proper constructors

--- src/main/java/br/unb/cic/witup/analysis/symbolic/SymInterfaceInvoke.java
+++ src/main/java/br/unb/cic/witup/analysis/symbolic/SymInterfaceInvoke.java
@@ -65,7 +65,7 @@ public final class SymInterfaceInvoke extends SymExpr {
     }
 
     if (newBase != base || newArgs != null) {
-      return new SymVirtualInvoke(
+      return new SymInterfaceInvoke(
           newBase, signature, returnsBoolean, newArgs != null ? newArgs : args);
     }
     return this;
@@ -93,7 +93,7 @@ public final class SymInterfaceInvoke extends SymExpr {
     }
 
     if (newBase != base || newArgs != null) {
-      return new SymVirtualInvoke(
+      return new SymInterfaceInvoke(
           newBase, signature, returnsBoolean, newArgs != null ? newArgs : args);
     }
     return this;
@@ -114,7 +114,15 @@ public final class SymInterfaceInvoke extends SymExpr {
 
   @Override
   public boolean contains(final String varName) {
-    return base.contains(varName);
+    if (base.contains(varName)) {
+      return true;
+    }
+    for (SymExpr arg : args) {
+      if (arg.contains(varName)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @Override
--- src/main/java/br/unb/cic/witup/analysis/symbolic/SymSpecialInvoke.java
+++ src/main/java/br/unb/cic/witup/analysis/symbolic/SymSpecialInvoke.java
@@ -65,7 +65,7 @@ public final class SymSpecialInvoke extends SymExpr {
     }
 
     if (newBase != base || newArgs != null) {
-      return new SymVirtualInvoke(
+      return new SymSpecialInvoke(
           newBase, signature, returnsBoolean, newArgs != null ? newArgs : args);
     }
     return this;
@@ -93,7 +93,7 @@ public final class SymSpecialInvoke extends SymExpr {
     }
 
     if (newBase != base || newArgs != null) {
-      return new SymVirtualInvoke(
+      return new SymSpecialInvoke(
           newBase, signature, returnsBoolean, newArgs != null ? newArgs : args);
     }
     return this;
